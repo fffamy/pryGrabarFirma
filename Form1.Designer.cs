4951ccb51a84625d7bce59f57da8900dd8ab5272
Federico Ahumada <fedee_crc@hotmail.com>
2024-03-26 18:10:49 -0300

    dejamos dos botones correctos, queda guardar la firma en un .png

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,90 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            pbFirma = new PictureBox();
+            pbFirma2 = new PictureBox();
+            btnDibujar = new Button();
+            btnGrabarFirma = new Button();
+            btnBorrar = new Button();
+            ((System.ComponentModel.ISupportInitialize)pbFirma).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pbFirma2).BeginInit();
+            SuspendLayout();
+            // 
+            // pbFirma
+            // 
+            pbFirma.BorderStyle = BorderStyle.FixedSingle;
+            pbFirma.Location = new Point(104, 68);
+            pbFirma.Name = "pbFirma";
+            pbFirma.Size = new Size(360, 326);
+            pbFirma.TabIndex = 0;
+            pbFirma.TabStop = false;
+            // 
+            // pbFirma2
+            // 
+            pbFirma2.BorderStyle = BorderStyle.FixedSingle;
+            pbFirma2.Location = new Point(599, 68);
+            pbFirma2.Name = "pbFirma2";
+            pbFirma2.Size = new Size(360, 326);
+            pbFirma2.TabIndex = 1;
+            pbFirma2.TabStop = false;
+            pbFirma2.MouseDown += pbFirma2_MouseDown;
+            pbFirma2.MouseMove += pbFirma2_MouseMove;
+            pbFirma2.MouseUp += pbFirma2_MouseUp;
+            // 
+            // btnDibujar
+            // 
+            btnDibujar.Location = new Point(104, 426);
+            btnDibujar.Name = "btnDibujar";
+            btnDibujar.Size = new Size(106, 72);
+            btnDibujar.TabIndex = 2;
+            btnDibujar.Text = "Dibujar";
+            btnDibujar.UseVisualStyleBackColor = true;
+            btnDibujar.Click += btnDibujar_Click;
+            // 
+            // btnGrabarFirma
+            // 
+            btnGrabarFirma.Location = new Point(853, 426);
+            btnGrabarFirma.Name = "btnGrabarFirma";
+            btnGrabarFirma.Size = new Size(106, 72);
+            btnGrabarFirma.TabIndex = 3;
+            btnGrabarFirma.Text = "Grabar";
+            btnGrabarFirma.UseVisualStyleBackColor = true;
+            // 
+            // btnBorrar
+            // 
+            btnBorrar.Location = new Point(479, 426);
+            btnBorrar.Name = "btnBorrar";
+            btnBorrar.Size = new Size(106, 72);
+            btnBorrar.TabIndex = 4;
+            btnBorrar.Text = "Borrar";
+            btnBorrar.UseVisualStyleBackColor = true;
+            btnBorrar.Click += btnBorrar_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(1058, 556);
+            Controls.Add(btnBorrar);
+            Controls.Add(btnGrabarFirma);
+            Controls.Add(btnDibujar);
+            Controls.Add(pbFirma2);
+            Controls.Add(pbFirma);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Grabar Firma";
+            ((System.ComponentModel.ISupportInitialize)pbFirma).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pbFirma2).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private PictureBox pbFirma;
+        private PictureBox pbFirma2;
+        private Button btnDibujar;
+        private Button btnGrabarFirma;
+        private Button btnBorrar;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,76 @@ namespace pryGrabarFirma
         {
             InitializeComponent();
         }
+
+        private void btnDibujar_Click(object sender, EventArgs e)
+        {
+            Graphics papel;
+            papel = pbFirma.CreateGraphics();
+
+            Pen lapicera = new Pen(Color.DarkGreen);
+
+            papel.DrawRectangle(lapicera, 10, 10, 100, 50);
+            papel.DrawRectangle(lapicera, 10, 75, 100, 100);
+
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            /*Configuro el boton borrar estableciendo la propiedad 'Image' del PictureBox como 'null'
+            La propiedad 'Image' del PictureBox representa la imagen que se muestra en el PictureBox, ya sea que la imagen fue
+            cargada desde archivos en el sistema, imágenes incrustadas en tu aplicación, o imágenes creadas dinámicamente en el tiempo de ejecución.*/
+            
+            pbFirma.Image = null;
+            pbFirma2.Image = null;
+        }
+        /*Creo la variable booleana 'click' y la inicializo en 'false' para indicar que inicialmente
+        no se está realizando ningún dibujo en el PictureBox. Cuando el usuario presiona el botón del ratón
+        dentro del PictureBox para comenzar a dibujar, se cambia el valor de esta variable a 'true' para indicar
+        que se está llevando a cabo el dibujo.*/
+
+        private bool click = false;
+
+        /*Creo la variable del tipo 'Point' que representa coordenadas en un espacio bidimensional
+         en este caso almacena y une el trazo del mouse para hacer el dibujo. */
+
+        private Point coordenadas;
+
+        private void pbFirma2_MouseDown(object sender, MouseEventArgs e)
+        {
+            //Empiezo a dibujar cuando hago click con el mouse
+
+            click = true;
+
+            //'e.Location' representa las coordenadas (x, y) del ratón en el control que generó el evento, en este caso, el PictureBox.
+            //Cuando el usuario mueve el ratón dentro del PictureBox, se activa el evento MouseMove
+            //y el parámetro 'e' contiene información sobre el evento, incluida la ubicación actual del cursor del ratón.
+
+            //Al asignar 'e.Location' a 'coordenadas', estamos actualizando la posición previa del cursor del ratón para que coincida con su posición actual.
+
+            coordenadas = e.Location;
+        }
+
+        private void pbFirma2_MouseMove(object sender, MouseEventArgs e)
+        {
+            //Dibuja la firma mientras muevo el mouse
+
+            if (click)
+            {
+                //Preguntar bien para que carajo se usa esto
+
+                using (Graphics g = pbFirma2.CreateGraphics())
+                {
+                    g.DrawLine(Pens.DarkGreen, coordenadas, e.Location);
+                    coordenadas = e.Location;
+                }
+            }
+        }
+
+        private void pbFirma2_MouseUp(object sender, MouseEventArgs e)
+        {
+            //Deja de dibujar cuando suelto el click
+
+            click = false;
+        }
     }
 }
